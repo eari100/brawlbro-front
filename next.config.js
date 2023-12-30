12c28d554d406b4558f609651dbbb0607cb184c0
Jaewook <eari100@naver.com>
2023-12-30 19:29:56 +0900

    cors: 브롤스타즈 api 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/brawlstars/:path*',
+                destination: 'https://api.brawlstars.com/:path*',
+            },
+        ]
+    },
+}
 
 module.exports = nextConfig
